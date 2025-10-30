c341fc99a83db7b01b1ed6ae0fc7c9cfd98156e8
Kr-EIVEN <harry4524@naver.com>
2025-10-30 18:17:10 +0900

    hot fix
    hot fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss";
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-const config: Config = withMT({
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
   ],
-  theme: {},
+  theme: {
+    extend: {},
+  },
   plugins: [],
-});
-
-export default config;
+};
